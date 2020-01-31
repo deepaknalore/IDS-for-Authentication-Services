98819e04f2e3a836cff9d072e341f8092729087b
Deepak Srinath <deepak.s.nalore@gmail.com>
2020-01-31 01:17:38 -0600

    Working

--- Simulation/IDSSimulation/app/routes.py
+++ Simulation/IDSSimulation/app/routes.py
@@ -1,7 +1,7 @@
 from app import app
 import sqlite3 as sql
 from haveibeenpwnd import check_email,check_password
-from flask import request, render_template, jsonify, make_response
+from flask import request, render_template, jsonify, make_response, abort
 
 @app.route('/')
 @app.route('/index')
@@ -10,6 +10,7 @@ def index():
 
 @app.route('/authenticate', methods = ['POST'])
 def authenticate():
+    print(request)
     if not request.json or not 'user' in request.json or not 'password' in request.json or not 'metadata' in request.json:
         abort(400)
     try:
